155e4de7c9c054ed4414aa3e0a771c26fcbc3dc6
EC2 Default User <ec2-user@ip-172-31-89-207.ec2.internal>
2021-09-06 04:04:17 +0000

    [update]3-2トップページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'blogs/index'
+  # get 'blogs/show'
+  # get 'blogs/new'
+  # get 'blogs/edit'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #↓上記４行get~にcreate,update,destroyを追加したものを１文で設定できる
+  resources:blogs
 end
