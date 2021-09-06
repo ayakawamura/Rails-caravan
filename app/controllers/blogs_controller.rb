6dc3d637939784491b377d9633eafd2819c5217f
EC2 Default User <ec2-user@ip-172-31-89-207.ec2.internal>
2021-09-06 12:14:49 +0000

    [update]3-3新規投稿画面

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,8 +6,24 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
+  end
+  
+  def create
+    blog=Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
   end
 
   def edit
   end
+  
+  private
+  # ストロングパラメータ(入力フォームからのデータを受け取る)
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
+  end
+ 
+  
+  
 end
