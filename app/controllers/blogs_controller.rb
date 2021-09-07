a0c1253fec519043d4be1d2473d84ad898f5f731
EC2 Default User <ec2-user@ip-172-31-89-207.ec2.internal>
2021-09-07 09:50:19 +0000

    [update]デバックツール

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,6 +7,8 @@ class BlogsController < ApplicationController
  
   def index
     @blogs=Blog.all
+     p "標準出力にのみ反映"
+    logger.debug("標準出力とログファイルに記録される")
   end
   
   def new
@@ -16,6 +18,7 @@ class BlogsController < ApplicationController
   
   def create
     @blog=Blog.new(blog_params)
+    binding.pry 
   # バリデーション設定　blog=>@blogへ修正
     if @blog.save
       redirect_to blog_path(@blog.id)
@@ -44,7 +47,7 @@ class BlogsController < ApplicationController
   private
   # ストロングパラメータ(入力フォームからのデータを受け取る)
   def blog_params
-    params.require(:blog).permit(:title, :categoly, :body)
+    params.require(:blog).permit(:title, :category, :body)
   end
  
   
