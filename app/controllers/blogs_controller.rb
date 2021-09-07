a339aac184d68eb4189819135c54c4179afc370e
EC2 Default User <ec2-user@ip-172-31-89-207.ec2.internal>
2021-09-07 07:20:19 +0000

    [update]3-6編集機能追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -19,7 +19,15 @@ class BlogsController < ApplicationController
     redirect_to blog_path(blog.id)
   end
 
+# 編集機能
   def edit
+    @blog=Blog.find(params[:id])
+  end
+  
+  def update
+    blog=Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
   end
   
   private
